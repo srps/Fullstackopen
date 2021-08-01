59729682eed794988173ad2546c72f3d571666de
srps <a_k_a_bouncer@hotmail.com>
2021-08-02 00:22:16 +0100

    Data for countries 2.14

--- React/part2/dataforcountries/src/App.js
+++ React/part2/dataforcountries/src/App.js
@@ -23,18 +23,47 @@ const Countries = ({countries, setFilter}) => {
   )
 }
 
-const Country = ({country}) => (
-  <div>
-    <h1>{country.name}</h1>
-    <div>Capital: {country.capital}</div>
-    <div>Population: {country.population}</div>
-    <h2>Languages</h2>
-      <ul>
-        {country.languages.map(language => <li key={language.iso639_2}>{language.name}</li>)}
-      </ul>
-    <img src={country.flag} alt="flag" width="200"/>
-  </div>
-)
+const Country = ({country}) => {
+  
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <div>Capital: {country.capital}</div>
+      <div>Population: {country.population}</div>
+      <h2>Languages</h2>
+        <ul>
+          {country.languages.map(language => <li key={language.iso639_2}>{language.name}</li>)}
+        </ul>
+      <img src={country.flag} alt="flag" width="200"/>
+      <CountryWeather country={country} />
+    </div>
+  )
+}
+
+const CountryWeather = ({country}) => {
+
+  const [ weather, setWeather ] = useState()
+  const weather_api_key = process.env.REACT_APP_WEATHER_API_KEY
+
+  console.log('country', country)
+  console.log('weather', weather)
+  console.log('api_key', weather_api_key)
+  console.log('env', process.env.REACT_APP_TITLE)
+  useEffect(() => {
+    axios.get(`http://api.weatherstack.com/current?access_key=${weather_api_key}&query=${country.capital}`).then(response => {
+      setWeather(response.data)
+    })},[country.capital, weather_api_key])
+
+  return typeof weather === 'undefined' ? (<></>) :
+  (
+    <>
+    <h2>Weather in {country.capital}</h2>
+      <div><b>Temperature:</b> {weather.current.temperature} Celsius</div>
+      <img src={weather.current.weather_icons[0]} alt="weather" width="100"/>
+      <div><b>Wind:</b> {weather.current.wind_speed} mph direction {weather.current.wind_dir}</div>
+    </>
+  )
+}
 
 const App = () => {
 
@@ -53,7 +82,7 @@ const App = () => {
   useEffect(() => {
     axios.get('https://restcountries.eu/rest/v2/all').then(response => {
       setCountries(response.data)
-  })},[])
+    })},[])
 
   return (
     <div>
